57a85299b0df674487a45d076b71969e7f27b28b
Matt Blethen <mattblethen@gmail.com>
2025-11-12 15:40:56 -0500

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import prefetch from '@astrojs/prefetch';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx(), sitemap(), prefetch()]
+});
